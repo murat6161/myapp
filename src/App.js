78c54ecdb9ccf7433a59ebdc86b7b8c856796ddd
Murat <mrtgms3561@gmail.com>
2021-03-19 13:21:43 +0200

    Card Style Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import User from './components/User'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Navbar title = "User App"/>
+        <hr/>
+
+        <User 
+        name = "Murat Gumus"
+        salary = "5000"
+        department = "Bilisim"/>
+
+    <User 
+        name = "Hakan Yilmaz"
+        salary = "4000"
+        department = "Pazarlama"/>
+
+      <User 
+        name = "Osman Turan"
+        salary = "6000"
+        department = "Uretim"/>    
+      </div>
+      
+    )
+  }
 }
-
-export default App;
---
+++ src/components/User.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+class User extends Component {
+    render() {
+
+        //Destructing
+        const {name, department, salary} = this.props
+        return (
+            <div className="col-md-8 mb-4">
+                <div className="card">
+                    <div className="card-header d-flex justify-content-between">
+                        <h4 className="d-inline">{name}</h4>
+                        <i className= "far fa-trash-alt" style={{cursor:"pointer"}}></i>
+                    </div>
+
+                    <div className="card-body">
+                        <p className="card-text">Maas : {salary}</p>
+                        <p className="card-text">Department : {department}</p>
+                    </div>
+
+                </div>
+
+
+            </div>
+        )
+    }
+}
+
+export default User
